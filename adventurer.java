347a4a4571649e402b620893d6667a74d675b1df
adifire1 <82788016+adifire1@users.noreply.github.com>
2022-09-22 23:45:26 -0600

    Update adventurer.java

--- adventurer.java
+++ adventurer.java
@@ -6,3 +6,29 @@ import java.util.Random;
 
 public class adventurer {
 }
+
+class brawler extends adventurer{
+  void spawn(){
+    System.out.println("Brawler spawned.");
+  }
+}
+
+class sneaker extends adventurer{
+    void spawn(){
+    System.out.println("Sneaker spawned.");
+  }
+}
+  
+ 
+class runner extends adventurer{
+    void spawn(){
+    System.out.println("Runner spawned.");
+  }
+}
+   
+   
+class thief extends adventurer{
+    void spawn(){
+    System.out.println("Thiefs spawned.");
+  }
+}
